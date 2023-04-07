2b86bfd00f7fa362e28e10a6cf94765692da3d5d
H4de <97474268+H4de421@users.noreply.github.com>
2023-04-07 12:46:02 +0100

    Update settings.py

--- djangoProject_school/settings.py
+++ djangoProject_school/settings.py
@@ -85,8 +85,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'Clement',
-        'PASSWORD': 'Loutre102003)',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': 'localhost',
     }
 }
